584bb883ac1e61907f27061a7a8f75a66138f8ce
Ron <imrondaramae@gmail.com>
2024-10-28 16:33:40 +0700

    Update APIs: User List

--- APIs/connection.js
+++ APIs/connection.js
@@ -8,7 +8,7 @@ const sequelize = new Sequelize(
         host: process.env.DB_HOST,
         port: process.env.DB_PORT,
         dialect: process.env.DB_DIALECT,
-        logging: false
+        logging: true
     }
 );
 
